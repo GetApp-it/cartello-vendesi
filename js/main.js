2fed91901d4e476ce6204e7a52ab4af31828acf7
Marco Gargano <mail@marcogargano.com>
2024-02-23 22:43:39 +0100

    Starting project...

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$("#nome").keyup(function () {
+	$("#nome-show").text($(this).val());
+});
+$("#cognome").keyup(function () {
+	$("#cognome-show").text($(this).val());
+});
+$("#tel").keyup(function () {
+	$("#tel-show").text($(this).val());
+});
+$("#desc1").keyup(function () {
+	$("#desc1-show").text($(this).val());
+});
+$("#desc2").keyup(function () {
+	$("#desc2-show").text($(this).val());
+});
+$("#desc3").keyup(function () {
+	$("#desc3-show").text($(this).val());
+});
